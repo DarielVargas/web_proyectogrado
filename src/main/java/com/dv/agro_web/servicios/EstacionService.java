f72c427772164ca59a0b70725bb2d788787bd117
Dariel Vargas <20181853@ce.pucmm.edu.do>
2026-03-07 14:52:37 -0400

    estaciones add

--- src/main/java/com/dv/agro_web/servicios/EstacionService.java
+++ src/main/java/com/dv/agro_web/servicios/EstacionService.java
@@ -3,6 +3,7 @@ package com.dv.agro_web.servicios;
 import com.dv.agro_web.entidades.Estacion;
 import com.dv.agro_web.repositorios.EstacionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,23 +17,31 @@ public class EstacionService {
         this.estacionRepository = estacionRepository;
     }
 
-    public List<Estacion> obtenerEstaciones() {
-        return estacionRepository.findAll();
+    public List<Estacion> obtenerEstacionesActivas() {
+        return estacionRepository.findAllActivas();
     }
 
-    public Optional<Estacion> obtenerEstacionPorId(Long id) {
-        return estacionRepository.findById(id);
+    public Optional<Estacion> obtenerEstacionActivaPorId(Long id) {
+        return estacionRepository.findActivaById(id);
     }
 
-    public Optional<Estacion> obtenerEstacionPorCodigo(String codigo) {
-        return estacionRepository.findByCodigoIgnoreCase(codigo);
+    public Optional<Estacion> obtenerEstacionActivaPorCodigo(String codigo) {
+        return estacionRepository.findActivaByCodigoIgnoreCase(codigo);
     }
 
-    public boolean existeCodigo(String codigo) {
-        return estacionRepository.existsByCodigoIgnoreCase(codigo);
+    public boolean existeCodigoActivo(String codigo) {
+        return estacionRepository.existsActivaByCodigoIgnoreCase(codigo);
     }
 
     public Estacion crearEstacion(Estacion estacion) {
+        if (estacion.getActiva() == null) {
+            estacion.setActiva(true);
+        }
         return estacionRepository.save(estacion);
+    }
+
+    @Transactional
+    public boolean desactivarEstacion(Long id) {
+        return estacionRepository.desactivarPorId(id) > 0;
     }
 }
