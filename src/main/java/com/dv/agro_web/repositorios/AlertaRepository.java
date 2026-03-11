e9c2726325106b665b8c72b1b1d4247909fd1f9b
Dariel vargas <20181853@ce.pucmm.edu.do>
2026-03-11 02:37:56 -0400

    Filtra alertas configuradas por estado activo

--- src/main/java/com/dv/agro_web/repositorios/AlertaRepository.java
+++ src/main/java/com/dv/agro_web/repositorios/AlertaRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 public interface AlertaRepository extends JpaRepository<Alerta, Long> {
     List<Alerta> findAllByOrderByFechaCreacionDescIdAlertaDesc();
 
+    List<Alerta> findAllByActivaTrueOrderByFechaCreacionDescIdAlertaDesc();
+
     List<Alerta> findAllByOrderByFechaCreacionAscIdAlertaAsc();
 
     long countByActivaTrue();
