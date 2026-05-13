fc3b20a70880a85afd13a14478f64b552ac5ead3
Dariel Vargas <20181853@ce.pucmm.edu.do>
2026-05-13 14:42:53 -0400

    seguimiento cultivo

--- src/main/java/com/dv/agro_web/repositorios/InformacionCultivoRepository.java
+++ src/main/java/com/dv/agro_web/repositorios/InformacionCultivoRepository.java
@@ -3,9 +3,12 @@ package com.dv.agro_web.repositorios;
 import com.dv.agro_web.entidades.InformacionCultivo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InformacionCultivoRepository extends JpaRepository<InformacionCultivo, Long> {
 
     Optional<InformacionCultivo> findFirstByOrderByIdAsc();
+
+    List<InformacionCultivo> findAllByOrderByIdDesc();
 }
