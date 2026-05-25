f6908efdab0fee32db035c228e836076710e90d9
Dariel Vargas <20181853@ce.pucmm.edu.do>
2026-05-25 01:08:18 -0400

    satelite

--- src/main/java/com/dv/agro_web/controllers/AnalisisSatelitalController.java
+++ src/main/java/com/dv/agro_web/controllers/AnalisisSatelitalController.java
@@ -16,7 +16,7 @@ public class AnalisisSatelitalController {
 
     @GetMapping("/analisis-satelital")
     public String verAnalisisSatelital(Model model) {
-        model.addAttribute("historialIndices", satelitalService.listarHistorial());
+        model.addAttribute("historialIndices", satelitalService.listarHistorialPresentacion());
         model.addAttribute("resumenSatelital", satelitalService.obtenerResumenActual());
         return "analisis-satelital";
     }
--- src/main/java/com/dv/agro_web/entidades/IndiceSatelital.java
+++ src/main/java/com/dv/agro_web/entidades/IndiceSatelital.java
@@ -15,7 +15,7 @@ public class IndiceSatelital {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(name = "fecha", nullable = false)
     private LocalDateTime fecha;
@@ -32,11 +32,11 @@ public class IndiceSatelital {
     @Column(name = "estado_hidrico")
     private String estadoHidrico;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
